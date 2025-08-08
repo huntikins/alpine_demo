4bfe3de92714dcbaff4591b17413d8a425cc2fe1
Hunter Trammell <htrammell@onenorth.com>
2025-08-08 11:30:15 -0500

    [feature] base demo content

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import Alpine from "alpinejs";
+
+document.addEventListener("alpine:init", () => {
+  // Register Components
+});
+
+// Register Alpine to the window
+window.Alpine = Alpine;
+Alpine.start();
